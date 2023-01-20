726342cbd65da02937b4a97c92b67abad9bc1797
Triantafyllos Xydis <97985533+triaxyd@users.noreply.github.com>
2023-01-20 14:25:53 +0200

    Add files via upload

---
+++ battleship/battleship/PlayerInfo.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace battleship
+{
+    public class PlayerInfo
+    {
+        public string PlayerName { get; set; }
+        public string PlayerDifficulty { get; set; }
+        public int PlayerWins { get; set; }
+        public int PlayerLoses { get; set; }
+        public int AverageRounds { get; set; }
+
+        public PlayerInfo(string name, string difficulty)
+        {
+            PlayerName = name;
+            PlayerDifficulty = difficulty;
+            PlayerWins = 0;
+            PlayerLoses = 0;
+            AverageRounds = 0;
+        }
+
+        
+
+    }
+}
